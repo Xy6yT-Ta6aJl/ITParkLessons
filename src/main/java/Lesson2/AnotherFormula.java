c630dbef5b69cacab2697bee703a69c8915ffbcb
Xy6yT-Ta6aJl <tarblade@gmail.com>
2021-11-07 01:23:13 +0300

    Добавил ещё 1 формулу

---
+++ src/main/java/Lesson2/AnotherFormula.java
@@ -0,0 +1,18 @@
+package Lesson2;
+
+import static java.lang.Math.pow;
+
+    // Формула Эквивалентности массы и энергии E = mc²
+
+public class AnotherFormula {
+    public static void main(String[] args) {
+
+
+        double e = greatFormula(92, 299_792_458);
+        System.out.println(e);
+
+
+    }public static double greatFormula(double m, double c){
+        return m * pow(c, 2);
+    }
+}
