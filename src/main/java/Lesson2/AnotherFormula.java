67d6f852a8a0a017c74892b49b5121f922ea6f29
Xy6yT-Ta6aJl <tarblade@gmail.com>
2021-11-07 02:52:17 +0300

    Исправил формулу выведя значение С в константу

--- src/main/java/Lesson2/AnotherFormula.java
+++ src/main/java/Lesson2/AnotherFormula.java
@@ -5,10 +5,12 @@ import static java.lang.Math.pow;
 // Формула Эквивалентности массы и энергии E = mc²
 
 public class AnotherFormula {
+    public static final long C = 299_792_458;
+
     public static void main(String[] args) {
 
 
-        double e = greatFormula(92, 299_792_458);
+        double e = greatFormula(92, C);
         System.out.println(e);
 
 
