b94d255ee85d6a63126edeea7874d1a215fee2ad
Xy6yT-Ta6aJl <tarblade@gmail.com>
2021-11-07 00:52:30 +0300

    Переделал на Java 17

---
+++ src/main/java/Lesson2/Formula.java
@@ -0,0 +1,21 @@
+package Lesson2;
+
+import static java.lang.Math.pow;
+
+    // Квадрат разности  (a - b)² = a² - 2ab + b²
+
+public class Formula {
+    public static void main(String[] args) {
+
+        double result = squaredOfDifference(35, 6);
+        System.out.println(result);
+
+
+    }
+
+    public static double squaredOfDifference(double a, double b) {
+
+        return pow((a - b), 2);
+    }
+}
+
---
+++ src/main/java/Lesson2/SimpleMath.java
@@ -0,0 +1,33 @@
+package Lesson2;
+
+public class SimpleMath {
+
+
+    public static void main(String[] args) {
+
+        int sum = addition(6, 8);
+        System.out.println(sum);
+        int difference = subtraction(55, 28);
+        System.out.println(difference);
+        int product = multiplication(22, 8);
+        System.out.println(product);
+        int quotient = division(21, 3);
+        System.out.println(quotient);
+    }
+
+    public static int addition(int arg1, int arg2) {
+        return arg1 + arg2;
+    }
+
+    public static int subtraction(int arg1, int arg2) {
+        return arg1 - arg2;
+    }
+
+    public static int multiplication(int arg1, int arg2) {
+        return arg1 * arg2;
+    }
+
+    public static int division(int arg1, int arg2) {
+        return arg1 / arg2;
+    }
+}
